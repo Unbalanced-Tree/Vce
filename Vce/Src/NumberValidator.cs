bf9796f0f14c3b4398df1a77566930a80b964623
Hungrywolf <33512936+hungrywoolf@users.noreply.github.com>
2024-10-05 12:22:05 +0530

    Check Number: IsNullOrZero for short, int, long, decimal, float, and double

---
+++ Vce/Src/NumberValidator.cs
@@ -0,0 +1,33 @@
+﻿namespace Vce
+{
+    public static class NumberValidator
+    {
+
+        // Optimized methods for common types
+        public static bool IsNullOrZero(this short? value)
+        {
+            return value == null || value == 0;
+        }
+        public static bool IsNullOrZero(this int? value)
+        {
+            return value == null || value == 0;
+        }
+        public static bool IsNullOrZero(this long? value)
+        {
+            return value == null || value == 0;
+        }
+
+        public static bool IsNullOrZero(this decimal? value)
+        {
+            return value == null || value == 0; 
+        }
+        public static bool IsNullOrZero(this float? value)
+        {
+            return value == null || value == 0;
+        }
+        public static bool IsNullOrZero(this double? value)
+        {
+            return value == null || value == 0;
+        }
+    }
+}
